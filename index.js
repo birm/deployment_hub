7e913ef4884ba81373343619113f0cf8a178d33a
Birm <birm@rbirm.us>
2018-01-31 10:29:26 -0500

    change sql, routes

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ sq3_init();
 // ENDPOINTS
 
 // new service server registry
-app.post("/new/service", function(req, res){
-    let stmt = db.prepare('insert into services values (?, ?)');
+app.post("/new/services", function(req, res){
+    let stmt = db.prepare('insert into services values (?, ?);');
     for (let service in req.body.services){
         stmt.run([req.body.host, services[service]]);
     }
@@ -33,7 +33,7 @@ app.post("/new/auth", function(req, res){
     req.body.pubkey
     // TODO randomly generate an ID
     let id = crypto.randomBytes(5).toString('hex');
-    let stmt = db.prepare('insert into services values (?, ?)');
+    let stmt = db.prepare('insert into auth values (?, ?);');
     // TODO handle failure "existing key" by trying again
     stmt.run([id, req.body.pubkey]);
     stmt.finalize();
@@ -41,25 +41,54 @@ app.post("/new/auth", function(req, res){
 });
 // new deployment variable
 app.post("/new/variable", function(req, res){
-    let stmt = db.prepare('insert into variables values (?, ?)');
+    let stmt = db.prepare('insert into variables values (?, ?);');
     stmt.run([req.body.name, req.body.variable]);
     stmt.finalize();
 });
+
+// new service server registry
+app.post("/update/services", function(req, res){
+    db.run('begin transaction;');
+    db.run('delete from services where host = ?;',[req.body.host] )
+    let stmt = db.prepare('insert into services values (?, ?);');
+    for (let service in req.body.services){
+        stmt.run([req.body.host, services[service]]);
+    }
+    db.run('commit;');
+    stmt.finalize();
+});
+// new auth proxy
+app.post("/update/auth", function(req, res){
+    req.body.host
+    req.body.pubkey
+    let stmt = db.prepare('replace into auth values (?, ?);');
+    // TODO handle failure "existing key" by trying again
+    stmt.run([id, req.body.pubkey]);
+    stmt.finalize();
+    // TODO return the id
+});
+// new deployment variable
+app.post("/update/variable", function(req, res){
+    let stmt = db.prepare('replace into variables values (?, ?);');
+    stmt.run([req.body.name, req.body.variable]);
+    stmt.finalize();
+});
+
 // list of services
-app.get("/services", function(req, res){
-    db.all('select * from services', (err,rows) => (res.json(rows)));
+app.get("/get/services", function(req, res){
+    db.all('select * from services;', (err,rows) => (res.json(rows)));
 });
 // pub key for auth proxy given id
-app.get("/key/:id", function(req, res){
-    db.get('select * from services where id = ?', [req.params.id], (err,row) => (res.json(row)));
+app.get("/get/key/:id", function(req, res){
+    db.get('select * from services where id = ?;', [req.params.id], (err,row) => (res.json(row)));
 });
 // get deployment variable
-app.get("/variable/:name", function(req, res){
-    db.get('select * from variables where name = ?', [req.params.name], (err,row) => (res.json(row)));
+app.get("/get/variables/one/:name", function(req, res){
+    db.get('select * from variables where name = ?;', [req.params.name], (err,row) => (res.json(row)));
 });
 // get deployment variables
-app.get("/variables", function(req, res){
-    db.all('select * from variables', (err,rows) => (res.json(rows)));
+app.get("/get/variables", function(req, res){
+    db.all('select * from variables;', (err,rows) => (res.json(rows)));
 });
 
 
