66400d7dc114ea07c5fb34e921ad07bffc8609d6
Ryan Birmingham <birm@rbirm.us>
2018-02-01 14:23:34 -0500

    remove old build method

--- build.sh
+++
@@ -1,9 +0,0 @@
-ADMIN_PW = $1
-# install npm, node
-
-apt-get --yes --force-yes install npm
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-apt-get install -y nodejs
-npm install
-# host app
-nodejs index.js $ADMIN_PW &&
